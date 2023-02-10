d4b5e10f5400fe2b968516e7198e2a11c1a80131
vinay badola <vinaybadola.cs18@gmail.com>
2023-02-11 00:37:09 +0530

    adding OTP generation and Reset-password

--- server/router/route.js
+++ server/router/route.js
@@ -3,7 +3,7 @@ const router = Router();
 
 // Importing all controllers
 import * as controller from '../controllers/appController.js'
-import Auth from '../middleware/auth.js';
+import Auth ,{localVariables} from '../middleware/auth.js';
 
 /** POST methods */
 router.route('/register').post(controller.register);
@@ -13,14 +13,14 @@ router.route('/register').post(controller.register);
 
 /**GET Methods  */
 router.route('/user/:username').get(controller.getUser)
-router.route('/generateOTP').get(controller.generateOTP)
+router.route('/generateOTP').get( controller.VerifyUser, localVariables, controller.generateOTP)
 router.route('/verifyOTP').get(controller.verifyOTP)
 router.route('createResetSession').get(controller.createResetSession)
 
 
 /**PUT Methods */
 router.route('/updateuser').put( Auth, controller.updateUser);    // to update the userProfile = 1st it will authenticate user then it will update user
-router.route('/resetPassword').put(controller.resetPassword); // Use to Reset Password
+router.route('/resetPassword').put( controller.VerifyUser, controller.resetPassword); // Use to Reset Password
 
 
 export default router;
